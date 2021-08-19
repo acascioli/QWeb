521e13ad3e2e5d29a7ffbc16f2e07ef0bbeabcec
Alessandro <alessandro@localhost.localdomain>
2021-08-19 17:35:54 +0200

    Copied elements from Explore app

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+
+import '../widgets/responsive.dart';
+import '../widgets/top_bar_contents.dart';
+import '../widgets/bottom_bar.dart';
+import '../widgets/web_scrollbar.dart';
+import '../widgets/carousel.dart';
+import '../widgets/destination_heading.dart';
+// import 'package:explore/widgets/explore_drawer.dart';
+import '../widgets/featured_heading.dart';
+import '../widgets/featured_tiles.dart';
+import '../widgets/floating_quick_access_bar.dart';
+
+class HomePage extends StatefulWidget {
+  static const String route = '/';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScrollController _scrollController;
+  double _scrollPosition = 0;
+  double _opacity = 0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      extendBodyBehindAppBar: true,
+      appBar: ResponsiveWidget.isSmallScreen(context)
+          ? AppBar(
+              backgroundColor:
+                  Theme.of(context).bottomAppBarColor.withOpacity(_opacity),
+              elevation: 0,
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.brightness_6),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  color: Theme.of(context).iconTheme.color,
+                  onPressed: () {
+                    EasyDynamicTheme.of(context).changeTheme();
+                  },
+                ),
+              ],
+              title: Text(
+                'EXPLORE',
+                style: TextStyle(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+            )
+          : AppBar(
+              backgroundColor:
+                  Theme.of(context).bottomAppBarColor.withOpacity(_opacity),
+              elevation: 0,
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.brightness_6),
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  color: Theme.of(context).iconTheme.color,
+                  onPressed: () {
+                    EasyDynamicTheme.of(context).changeTheme();
+                  },
+                ),
+              ],
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  "assets/images/BMW_logo.png",
+                ),
+              ),
+              title: Text(
+                'EXPLORE',
+                style: TextStyle(
+                  color: Colors.blueGrey[100],
+                  fontSize: 20,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+      // drawer: ExploreDrawer(),
+      body: WebScrollbar(
+        color: Colors.blueGrey,
+        backgroundColor: Colors.blueGrey.withOpacity(0.3),
+        width: 10,
+        heightFraction: 0.3,
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          physics: ClampingScrollPhysics(),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    child: SizedBox(
+                      height: screenSize.height * 0.45,
+                      width: screenSize.width,
+                      child: Image.asset(
+                        'assets/images/cover.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      FloatingQuickAccessBar(screenSize: screenSize),
+                      Container(
+                        child: Column(
+                          children: [
+                            FeaturedHeading(
+                              screenSize: screenSize,
+                            ),
+                            FeaturedTiles(screenSize: screenSize)
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              DestinationHeading(screenSize: screenSize),
+              DestinationCarousel(),
+              SizedBox(height: screenSize.height / 10),
+              BottomBar(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
